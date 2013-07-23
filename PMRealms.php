aecfb757b7be8776d132b520b2ee7307da2c847a
shadowdude246 <shadowdude246@gmail.com>
2013-07-22 23:41:26 -0400

    Update PMRealms.php

--- PMRealms.php
+++ PMRealms.php
@@ -19,9 +19,9 @@ class PMRealms implements Plugin{
 	
 	public function init(){
 		$this->config = new Config($this->api->plugin->configPath($this)."config.yml", CONFIG_YAML, array(
-			"ownerName" => "",
-			"externalAddress" => "",
-			"externalPort" => "",
+			"ownerName" => "Slow_Rider",
+			"externalAddress" => "70.240.218.57",
+			"externalPort" => "19132",
 		));
 		
 		$error = 0;
